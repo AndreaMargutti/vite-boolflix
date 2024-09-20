d216e8a004a0171cd3390237020ce5c6d92b717b
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-20 11:21:08 +0200

    first_commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+// Imported Bootstraps
+@use "../node_modules/bootstrap/scss/bootstrap.scss";
+//Imported General CSS
+@use "./style/general.scss";
+</style>
