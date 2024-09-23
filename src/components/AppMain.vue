eca956ce00ba5657cc8bdf48d8ce0ace0975f271
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-23 22:05:25 +0200

    comments of section in ./components

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,36 +11,40 @@ export default {
 </script>
 
 <template>
+  <!--Inizio sezione film-->
   <section>
     <h1>Film</h1>
+    <!--Creo un "contatore per dire quanti risultati sono stati trovati"-->
     <p>I Risultati trovati sono: {{ store.findedFilms.length }}</p>
-    <ul v-show="store.findedFilms.length > 0">
-      <li v-for="(film, i ) in store.findedFilms" :key="i">
-        {{ store.findedFilms[i].title }}
-        {{ store.findedFilms[i].original_title }}
-        <div id="lang-flag" v-if="store.findedFilms[i].original_language === 'en'">
+    <ul v-show="store.findedFilms.length > 0"> <!--Mostriamo la lista sono se sono stati trovati dei risultati-->
+      <li v-for="(film, i ) in store.findedFilms" :key="i"> <!--Generiamo un li per ogni elemento dell'array-->
+        {{ store.findedFilms[i].title }} <!--Inseriamo il titolo del film-->
+        {{ store.findedFilms[i].original_title }} <!--Inseriamo il titolo originale-->
+        <div id="lang-flag" v-if="store.findedFilms[i].original_language === 'en'"> <!--Se lingua inglese allora mettimi la bandiera inglese-->
           <img src="../assets/img/uk_flag.svg" alt="uk-flag ">
         </div>
-        <div id="lang-flag" v-else="store.findedFilms[i].original_language === 'it'">
+        <div id="lang-flag" v-else="store.findedFilms[i].original_language === 'it'"> <!--Se lingua italiana allora metti la bandiera italiana-->
           <img src="../assets/img/ita_flag.png" alt="ita-flag">
         </div>
-        {{ store.findedFilms[i].vote_average }}
+        {{ store.findedFilms[i].vote_average }} <!--Inserisco il voto del film-->
       </li>
     </ul>
   </section>
+  <!--Inizio sezione serie tv-->
   <section>
     <h1>Serie TV</h1>
+    <!--Creo un "contatore per dire quanti risultati sono stati trovati"-->
     <p>I Risultati trovati sono: {{ store.findedSeries.length }}</p>
-    <ul v-show="store.findedSeries.length > 0">
-      <li v-for="(serie, j) in store.findedSeries" :key="j">
-        {{ store.findedSeries[j].original_name }}
-        <div id="lang-flag" v-if="store.findedSeries[j].original_language === 'en'">
+    <ul v-show="store.findedSeries.length > 0">  <!--Mostriamo la lista sono se sono stati trovati dei risultati-->
+      <li v-for="(serie, j) in store.findedSeries" :key="j"> <!--Generiamo un li per ogni elemento dell'array-->
+        {{ store.findedSeries[j].original_name }} <!--Inseriamo il titolo della serie tv-->
+        <div id="lang-flag" v-if="store.findedSeries[j].original_language === 'en'"> <!--Se lingua inglese allora mettimi la bandiera inglese-->
           <img src="../assets/img/uk_flag.svg" alt="uk-flag ">
         </div>
-        <div id="lang-flag" v-else="store.findedSeries[j].original_language === 'it'">
+        <div id="lang-flag" v-else="store.findedSeries[j].original_language === 'it'"> <!--Se lingua italiana allora metti la bandiera italiana-->
           <img src="../assets/img/ita_flag.png" alt="ita-flag">
         </div>
-        {{ store.findedSeries[j].vote_average }}
+        {{ store.findedSeries[j].vote_average }} <!--Inserisco il voto della serie tv-->
       </li>
     </ul>
   </section>
--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -5,16 +5,19 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      //importo la store
       store,
-      apiUrl: "https://api.themoviedb.org/3/search/movie?",
-      apiSeriesUrl: "https://api.themoviedb.org/3/search/tv"
+      apiUrl: "https://api.themoviedb.org/3/search/movie?", //link per API film
+      apiSeriesUrl: "https://api.themoviedb.org/3/search/tv" //link per API serie tv
     }
   },
 
   methods: {
+    //Evento per il click del bottone
     clickButton(){
       console.log('Bottone Cliccato');
       console.log(store.searchedInput);
+      //faccio partire i metodi per film e serie tv
       this.getFilms();
       this.getSeries();
     },
@@ -23,13 +26,13 @@ export default {
     getFilms(){
       axios.get(this.apiUrl, {
         params: {
-          api_key: "8e0805322dd3b50b2e8d4df4c08bfc3d",
-          query: store.searchedInput,
+          api_key: "8e0805322dd3b50b2e8d4df4c08bfc3d", //chiave necessaria per far funzionare l'api (guarda documentazione)
+          query: store.searchedInput, //input inserito dall'utente
       }})
       .then((response) => {
         //console.log(response.data.results);
-        store.findedFilms = response.data.results
-        //console.log(store.findedFilms); //Inserisco i film con il campo di ricerca corretto nell'array
+        store.findedFilms = response.data.results //Inserisco i film con il campo di ricerca corretto nell'array
+        //console.log(store.findedFilms);
       })
       .catch((error) => {
         console.log(error);
@@ -46,7 +49,7 @@ export default {
       .then((response) => {
         //console.log(response.data.results);
         store.findedSeries = response.data.results
-        console.log(response.data.results); //Inserisco i film con il campo di ricerca corretto nell'array
+        console.log(response.data.results); //Inserisco le serie con il campo di ricerca corretto nell'array
       })
       .catch((error) => {
         console.log(error);
@@ -60,7 +63,9 @@ export default {
 <template>
   <h1>SearchInput</h1>
   <div>
+    <!--Input di ricerca-->
     <input type="text" v-model="store.searchedInput">
+    <!--Bottone che fa partire il metodo al suo click-->
     <button @click="clickButton">Cerca</button>
   </div>
 </template>
