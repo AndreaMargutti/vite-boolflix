2b78e0d3461d4b53e21f62fc061f7d6014d390bf
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-23 15:52:58 +0200

    added tv series list

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <template>
-  <h1>Risultati di ricerca</h1>
+  <h1>Film</h1>
   <ul>
     <li v-for="(media, i ) in store.findedFilms" :key="i">
       {{ store.findedFilms[i].title }}
@@ -23,6 +23,19 @@ export default {
         <img src="../assets/img/ita_flag.png" alt="ita-flag">
       </div>
       {{ store.findedFilms[i].vote_average }}
+    </li>
+  </ul>
+  <h1>Serie TV</h1>
+  <ul>
+    <li v-for="(serie, j) in store.findedSeries" :key="j">
+      {{ store.findedSeries[j].original_name }}
+      <div id="lang-flag" v-if="store.findedSeries[j].original_language === 'en'">
+        <img src="../assets/img/uk_flag.svg" alt="uk-flag ">
+      </div>
+      <div id="lang-flag" v-else="store.findedSeries[j].original_language === 'it'">
+        <img src="../assets/img/ita_flag.png" alt="ita-flag">
+      </div>
+      {{ store.findedSeries[j].vote_average }}
     </li>
   </ul>
 </template>
